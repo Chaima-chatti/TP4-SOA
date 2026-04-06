4b491d44b225187e5a6eab6bb52935c3a560b53e
Chaima-chatti <chaima.chatti_1@polytechnicien.tn>
2026-04-06 14:43:44 +0200

    2nd commit

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const db = new sqlite3.Database('./maBaseDeDonnees.sqlite', sqlite3.OPEN_READWRI
     db.run(`CREATE TABLE IF NOT EXISTS personnes (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       nom TEXT NOT NULL,
-      adresse TEXT
+      adresse TEXT 
     )`, (err) => {
       if (err) {
         console.error(err.message);
